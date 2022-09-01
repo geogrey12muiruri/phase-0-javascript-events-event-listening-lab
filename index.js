db4684385d6fb03093a2ed8915aa5613309ad69a
geogrey12muiruri <greggambugua@gmail.com>
2022-09-01 20:59:34 +0300

    completed the assaignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    let input;
+    input = document.getElementById('input')
+    input.addEventListener('click', () => {
+        alert('I was clicked');
+    })
+  }
